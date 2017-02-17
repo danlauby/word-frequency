0e8ffb8a6209812dd870157517beccaa2b0ca3dc
danlauby <dmlauby@gmail.com>
2017-02-17 13:42:00 -0800

    Add properties, getters and setters to RepeatCounter class

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,6 +1,35 @@
 <?php
     class RepeatCounter
     {
+        private $word;
+        private $sentence;
+
+        function __construct($new_word, $new_sentence)
+        {
+            $this->word = $new_word;
+            $this->sentence = $new_sentence;
+        }
+
+        function setWord($new_word)
+        {
+            $this->word = (string) $new_word;
+        }
+
+        function getWord()
+        {
+            return $this->word;
+        }
+
+        function setSentence($new_sentence)
+        {
+            $this->sentence = (string) $new_sentence;
+        }
+
+        function getSentence()
+        {
+            return $this->sentence;
+        }
+
         function countRepeats($input_word, $input_sentence)
         {
             $count = 0;
