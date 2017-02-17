64f57f0a4e0957b8c5649a6c1d22934080b3385e
danlauby <dmlauby@gmail.com>
2017-02-17 09:22:26 -0800

    Test string for lowercase, pass

---
+++ src/WordFrequency.php
@@ -0,0 +1,11 @@
+<?php
+    class WordFrequency
+    {
+        function wordMatch($input)
+        {
+            $to_lower_case = strtolower($input);
+            return $to_lower_case;
+        }
+
+    }
+ ?>
---
+++ tests/WordFrequencyTest.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "src/WordFrequency.php";
+
+    class WordFrequencyTest extends PHPUnit_Framework_TestCase
+    {
+        function test_toLowercase()
+        {
+            // Arrange
+            $test_WordFrequency = new WordFrequency;
+            $input = 'To improve is to change';
+
+            // Act
+            $result = $test_WordFrequency->wordMatch($input);
+
+            //Assert
+            $this->assertEquals('to improve is to change', $result);
+        }
+    }
+ ?>
